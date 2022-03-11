648591242920a09bb3b829afe5f9b7b6213b265a
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-11 12:44:46 -0800

    added additional macro indicator

--- client/src/components/Macro.js
+++ client/src/components/Macro.js
@@ -15,10 +15,20 @@ export default function Macro() {
             Use market-implied indicators to gauge the liklihood of a recession. Below, we see that any time the 2-year US Treasury yield has exceeded the 10-year (<em>greater than 1.0%</em>), we have experienced a recession thereafter.
           </p>
         </div>
-        <div className="w-full md:w-11/12 mx-auto mt-10">
+        <div className="w-full md:w-11/12 mx-auto my-10">
           <iframe 
             className="w-full h-80 my-10"
             src="https://app.koyfin.com/share/b74c000ab3/simple"
+            frameBorder="0">
+          </iframe>
+        </div>
+        <div className="w-full md:w-11/12 mx-auto my-10">
+          <p className="max-w-prose mt-8 mx-auto text-xl text-center text-gray-500 leading-8">
+            Additionally, the widening of high-yield spreads (blue line) has coincided with stock market pullbacks - especially in small cap stocks (orange line).
+          </p>
+          <iframe 
+            className="w-full h-80 my-10"
+            src="https://app.koyfin.com/share/7c899f8b83/simple" 
             frameBorder="0">
           </iframe>
         </div>
