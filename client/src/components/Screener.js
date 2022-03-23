00c2e003029db5e041948894e3ec9f98344de034
sean-vdw <62733062+sean-vdw@users.noreply.github.com>
2022-03-23 12:55:44 -0700

    updated screener button

--- client/src/components/Screener.js
+++ client/src/components/Screener.js
@@ -58,7 +58,7 @@ export default function Screener() {
               onClick={handleGetFinancials}
               className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-blue-700 bg-blue-100 hover:bg-blue-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
             >
-              Update List
+              Run Screener
             </button>
             { loading ? <img src={Loader} className='ml-2 my-2 h-7 w-7 animate-spin'></img> : null }
           </div>
